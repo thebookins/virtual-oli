36ce22c540ec5bd2324d9d26dbfa0e403ce6b117
Paul Dickens <dickens.paul@gmail.com>
2018-11-24 22:35:38 +1100

    removed excessive logging

--- server/cgm/index.js
+++ server/cgm/index.js
@@ -4,7 +4,6 @@ module.exports = (t1d) => {
   const eventEmitter = new events.EventEmitter();
 
   setInterval(() => {
-    console.log(`emitteing glucose: ${t1d.sense()}`)
     eventEmitter.emit('glucose', t1d.sense());
   }, 1000);
 
--- server/cgmIO.js
+++ server/cgmIO.js
@@ -1,7 +1,6 @@
 module.exports = (io, cgm) => {
   const nsp = io.of('/cgm');
   cgm.on('glucose', (glucose) => {
-    console.log(`got glucose: ${glucose}`);
     nsp.emit('message', {glucose, readDate: new Date()});
   })
 }
