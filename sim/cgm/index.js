0279d06759d19c2c79badbb36a5aacd96bf83b85
Paul Dickens <dickens.paul@gmail.com>
2018-12-09 10:35:58 +1100

    add empty sound field

--- sim/cgm/index.js
+++ sim/cgm/index.js
@@ -30,6 +30,7 @@ module.exports = () => {
 
     var note = new apn.Notification();
     note.contentAvailable = 1;
+    note.sound = "";
     note.topic = "com.8ZWMLSD6JG.loopkit.Loop"
 
     apnProvider.send(note, deviceToken).then( (result) => {
