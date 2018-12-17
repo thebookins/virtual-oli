12ee71c3a56fab5644ae96a9cb7e2a110eca00d1
Paul Dickens <dickens.paul@gmail.com>
2018-12-17 17:56:37 +1100

    move stuff to worker

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
+const MongoClient = require('mongodb').MongoClient;
+// var ObjectID = mongodb.ObjectID;
 
 const socketIO = require('socket.io');
 
@@ -13,11 +13,10 @@ var distDir = __dirname + "/dist/virtual-oli/";
 app.use(express.static(distDir));
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
+let db;
 
 // Connect to the database before starting the application server.
-// TODO: try async / await here
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, client) {
+MongoClient.connect(process.env.MONGODB_URI, function (err, client) {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -27,6 +26,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, client) {
   db = client.db();
   console.log("Database connection ready");
 
+  // NOTE: THIS SHOULD BE DELETED; JUST FOR TESTING
   db.collection('status').update({}, {$inc: {reservoir: 10}}, {upsert: true});
 
   // Initialize the app.
@@ -34,150 +34,94 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, client) {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
+});
+
+// const io = socketIO(server);
+
+const MEALS = [
+  { date: new Date(), value: 6 },
+  { date: new Date(), carbs: 5 }
+];
+
+// // t1d
+// const t1d = require('./sim/t1d')();
+
+// // cgm
+// const cgm = require('./sim/cgm')(db);
+// const cgmAPI = require('./sim/cgm/io')(io, cgm);
+//
+// // pump
+// const pump = require('./sim/pump')();
+// const pumpAPI = require('./sim/pump/io')(io, pump, db);
+
+// // hook up t1d, pump and cgm
+// t1d.attachPump(pump);
+// t1d.attachCGM(cgm);
+
+// io.on('connection', (socket) => {
+//   console.log('Client connected');
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+// });
+
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+//setTimeout(() => t1d.addPump(pump), 6000);
 
 
-  const io = socketIO(server);
-  const MEALS = [
-    { date: new Date(), value: 6 },
-    { date: new Date(), carbs: 5 }
-  ];
+/*  "/api/meals"
+ *    GET: finds all meals
+ *    POST: adds a new meal
+ */
 
-  // // t1d
-  // const t1d = require('./sim/t1d')();
+app.get("/api/meals", function(req, res) {
+  res.status(200).json(MEALS);
+});
 
-  // // cgm
-  // const cgm = require('./sim/cgm')(db);
-  // const cgmAPI = require('./sim/cgm/io')(io, cgm);
-  //
-  // // pump
-  // const pump = require('./sim/pump')();
-  // const pumpAPI = require('./sim/pump/io')(io, pump, db);
+app.post("/api/meals", function(req, res) {
+  var newMeal = req.body;
 
-  // // hook up t1d, pump and cgm
-  // t1d.attachPump(pump);
-  // t1d.attachCGM(cgm);
+  if (!req.body.carbs) {
+    handleError(res, "Invalid user input", "Must provide carbs.", 400);
+  }
 
-  io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
-  });
+  // t1d.eat(newMeal);
+  res.status(201).json(newMeal);
+});
 
-  setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+/*  "/api/cgm"
+ *    GET: returns the last three hours of glucose
+ */
 
-  //setTimeout(() => t1d.addPump(pump), 6000);
+// CGM endpoints
+// app.get('/api/cgm', cgmAPI.latest);
+app.get('/api/cgm', function(req, res) {
+  res.json({});
+});
 
-
-  /*  "/api/meals"
-   *    GET: finds all meals
-   *    POST: adds a new meal
-   */
-
-  app.get("/api/meals", function(req, res) {
-    res.status(200).json(MEALS);
-  });
-
-  app.post("/api/meals", function(req, res) {
-    var newMeal = req.body;
-
-    if (!req.body.carbs) {
-      handleError(res, "Invalid user input", "Must provide carbs.", 400);
-    }
-
-    // t1d.eat(newMeal);
-    res.status(201).json(newMeal);
-  });
-
-
-  // NOTE: queue stuff: destined for a worker process - here now to save money!!!
-  var q = 'tasks';
-  var url = process.env.CLOUDAMQP_URL || "amqp://localhost";
-  var open = require('amqplib').connect(url);
-  var apn = require('apn');
-
-  const options = {
-    token: {
-      key: __dirname + "/sim/cgm/AuthKey_23NRN4PHVP.p8",
-      keyId: "23NRN4PHVP",
-      teamId: "8ZWMLSD6JG"
-    },
-    production: false
-  };
-
-  const apnProvider = new apn.Provider(options);
-
-  var glucose;
-
-  // Consumer
-  open.then(function(conn) {
-    var ok = conn.createChannel();
-    ok = ok.then(function(ch) {
-      ch.assertQueue(q);
-      ch.consume(q, function(msg) {
-        if (msg !== null) {
-          console.log(`received message: ${msg.content.toString()}`);
-          ch.ack(msg);
-
-          glucose = {
-            readDate: new Date(),
-            glucose: 6.0
-          }
-
-          let deviceToken = "c31ce3c0585db5744839accc7c6a6d42eb7649d0b9608b8df1757be077947240"
-
-          var note = new apn.Notification();
-          note.contentAvailable = 1;
-          // NOTE: probably not necessary to set this field...
-          // note.sound = "";
-          note.topic = "com.8ZWMLSD6JG.loopkit.Loop"
-
-          apnProvider.send(note, deviceToken).then( (result) => {
-            console.log(JSON.stringify(result));
-            // see documentation for an explanation of result
-          });
-
-        }
-      });
-    });
-    return ok;
-  }).then(null, console.warn);
-
-
-  /*  "/api/cgm"
-   *    GET: returns the last three hours of glucose
-   */
-
-  // CGM endpoints
-  // app.get('/api/cgm', cgmAPI.latest);
-  app.get('/api/cgm', function(req, res) {
-    res.json(glucose);
-  });
-
-  //
-  // // pump endpoints
-  // app.get('/api/pump', pumpAPI.history);
-  app.get('/api/pump', function(req, res) {
-    res.json([]);
-  });
-  // app.post('/api/pump', pumpAPI.post);
-  // app.get('/api/pump/status', pumpAPI.status);
-  app.get('/api/pump/status', function(req, res) {
-    const status = {
-      // clock,
-      // batteryVolts,
-      // batteryStatus,
-      suspended: false,
-      bolusing: false,
-      reservoir: 80.5,
-      // model,
-      // pumpID
-    }
-    res.json(status);
-  },
-);
+//
+// // pump endpoints
+// app.get('/api/pump', pumpAPI.history);
+app.get('/api/pump', function(req, res) {
+  res.json([]);
+});
+// app.post('/api/pump', pumpAPI.post);
+// app.get('/api/pump/status', pumpAPI.status);
+app.get('/api/pump/status', function(req, res) {
+  const status = {
+    // clock,
+    // batteryVolts,
+    // batteryStatus,
+    suspended: false,
+    bolusing: false,
+    reservoir: 80.5,
+    // model,
+    // pumpID
+  }
+  res.json(status);
+});
   // app.get('/api/pump/history', ???)
   // app.get('/api/pump/basal', ???)
   // app.post('/api/pump/basal', ???)
   // app.post('/api/pump/temp')
   //
   // app.post('bolus', ???)
-});
--- worker.js
+++ worker.js
@@ -1,50 +1,94 @@
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
+var q = 'tasks';
+var url = process.env.CLOUDAMQP_URL || "amqp://localhost";
+var open = require('amqplib').connect(url);
+var apn = require('apn');
+
+const options = {
+  token: {
+    key: __dirname + "/sim/cgm/AuthKey_23NRN4PHVP.p8",
+    keyId: "23NRN4PHVP",
+    teamId: "8ZWMLSD6JG"
+  },
+  production: false
+};
+
+const apnProvider = new apn.Provider(options);
+
+
+
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
-// TODO: try async / await here
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, client) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
+
+// // TODO: try async / await here
+// mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, client) {
+//   if (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
+//
+//   // Save database object from the callback for reuse.
+//   db = client.db();
+//   console.log("Database connection ready");
+//
+//
+//   // TODO: implement step functions for pump, t1d and cgm
+//   // step() will deal with any pending commands (bolus, temp basal, suspends etc),
+//   // bring the state up to the current time, and save the state back to the db
+//   pump.step();
+//   t1d.step();
+//   cgm.step();
+//
+//   // ALTERNATIVELY:
+//   // pump.step();
+//   // t1d.step(pump.insulin)
+//   // cgm.step(t1d.glucose)
+// });
+
+let glucose;
+
+// Consumer
+open.then(function(conn) {
+  var ok = conn.createChannel();
+  ok = ok.then(function(ch) {
+    ch.assertQueue(q);
+    ch.consume(q, function(msg) {
+      if (msg !== null) {
+        console.log(`received message: ${msg.content.toString()}`);
+        ch.ack(msg);
+
+        glucose = {
+          readDate: new Date(),
+          glucose: 6.0
+        }
+
+        let deviceToken = "c31ce3c0585db5744839accc7c6a6d42eb7649d0b9608b8df1757be077947240"
+
+        var note = new apn.Notification();
+        note.contentAvailable = 1;
+        // NOTE: probably not necessary to set this field...
+        // note.sound = "";
+        note.topic = "com.8ZWMLSD6JG.loopkit.Loop"
+
+        apnProvider.send(note, deviceToken).then( (result) => {
+          console.log(JSON.stringify(result));
+          // see documentation for an explanation of result
+        });
+
+      }
+    });
+  });
+  return ok;
+}).then(null, console.warn);
+
+module.exports = {
+  get glucose() {
+    return glucose;
   }
-
-  // Save database object from the callback for reuse.
-  db = client.db();
-  console.log("Database connection ready");
-
-  // t1d
-  // TODO: this constructor will load a new t1d from the database
-  const t1d = require('./sim/t1d')(db);
-
-  // cgm
-  // TODO: this constructor will load a new cgm from the database
-  const cgm = require('./sim/cgm')(db);
-
-  // pump
-  // TODO: this construcor will load a new pump from the database
-  const pump = require('./sim/pump')(db);
-
-  // hook up t1d, pump and cgm
-  t1d.attachPump(pump);
-  t1d.attachCGM(cgm);
-
-  // TODO: implement step functions for pump, t1d and cgm
-  // step() will deal with any pending commands (bolus, temp basal, suspends etc),
-  // bring the state up to the current time, and save the state back to the db
-  pump.step();
-  t1d.step();
-  cgm.step();
-
-  // ALTERNATIVELY:
-  // pump.step();
-  // t1d.step(pump.insulin)
-  // cgm.step(t1d.glucose)
-});
-
-
+}
 // NOTES:
 // The worker needs to step the cgm every 5 minutes only. It will get the glucose
 // from the t1d, save the reading to the db and send an APN. It will also update
